f972ae311eb1f65fc1555662a245b27da09968e0
TimothyYeou <timyeou1234@hotmail.com>
2017-05-12 15:11:20 +0800

    Add feature

---
+++ BT_DV_iOS/BT_DV_iOS/BLEViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BLEViewController.swift
+//  BT_DV_iOS
+//
+//  Created by YeouTimothy on 2017/5/12.
+//  Copyright © 2017年 VictorBasic. All rights reserved.
+//
+
+import UIKit
+
+class BLEViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
